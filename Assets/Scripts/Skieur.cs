63d16f99164c58c755f545c2443acaa889a3e30d
hwangdavid2005-ui <hwangdavid2005@gmail.com>
2026-03-06 17:50:03 -0500

    FINISHHHH
    Les panels ne marchent pas mais je suis fatigué.

--- Assets/Scripts/Skieur.cs
+++ Assets/Scripts/Skieur.cs
@@ -7,16 +7,16 @@ using static Unity.VisualScripting.Member;
 
 public class Skieur : MonoBehaviour
 {
-    //===== Variables publiques
+    
     public float vitesse;
 
     public InputAction onDeplacementVertical;
     public InputAction onDeplacementHorizontal;
 
-    //===== UI
+ 
     public TMP_Text textePointage;
 
-    //===== Variables privées
+    
     Rigidbody2D rigid;
 
     private Transform visuel;
@@ -28,6 +28,9 @@ public class Skieur : MonoBehaviour
     public bool estMort = false;
 
     public int points = 0; 
+
+  
+
     void Start()
     {
         points = 0;
@@ -36,7 +39,8 @@ public class Skieur : MonoBehaviour
         visuel = transform.Find("Visuel");
     }
 
-    // Utiliser ces fonctions pour activer et désactiver les InputActions
+    
+    
     private void OnEnable()
     {
         onDeplacementHorizontal.Enable();
@@ -76,67 +80,66 @@ public class Skieur : MonoBehaviour
 
     }
 
-    //Ajouter une méthode OnCollisionEnter2D
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Si le tag du gameobject de la collision est "Obstacle" et que le joueur n'est pas mort
-        if (estMort == false && collision.gameObject.tag == "Yeti")
+        
+        if (estMort == false && collision.gameObject.tag == "Danger")
         {
-            //// Affecter true à estMort
+            
+           
             estMort = true;
-            //// Déclencher la fonction Mourir
+            
             Mourir();
-            //// Déclencher la fonction DeconnecterCamera
+            
             DeconnecterCamera();
         }
 
     }
-    //Fin OnCollisionEnter2D
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    // Si le tag du gameobject de la collision est "Etoile" et que le joueur n'est pas mort
-    //    if (estMort == false && collision.gameObject.tag == "Bonhomme")
-    //    {
-    //        //// Accéder au script "Bonhommes"
-    //        Bonhommes bonhomme = collision.GetComponent<Bonhommes>();
-    //        //// Déclencher la fonction publique Cacher des Bonhommes
-    //        bonhomme.Cacher();
-    //        //// Ajouter des points
-    //        //points += 1;
-    //        //textePointage.text = $"points : {points}";
-    //    }
 
-    //}
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Collision détectée avec : " + collision.gameObject.name);
-        // Si le tag du gameobject de la collision est "Portail" et que le joueur n'est pas mort
+        Debug.Log("Collision dÃ©tectÃ©e avec : " + collision.gameObject.name);
+
+       
+        if (estMort == false && collision.gameObject.tag == "Bonhomme")
+        {
+            Bonhommes bonhomme = collision.GetComponent<Bonhommes>();
+
+            bonhomme.Cacher();
+            
+
+            points += 1;
+            textePointage.text = $"points : {points}";
+        }
+
+
         if (estMort == false && collision.gameObject.tag == "Portail")
         {
-            
             points += 1;
-            textePointage.text = $"points : { points }";
+            textePointage.text = $"points : {points}";
         }
-        //// Mettre à jour le texte du UI
 
     }
 
     void Mourir()
     {
-        // Enlever la contrainte de rotation
+  
         rigid.constraints = RigidbodyConstraints2D.None;
-        // Ajouter une vélocité de rotation
-        rigid.angularVelocity = 100f;
-        // Déclencher la fonction RedemarrerScene après 3sec
+      
+        
+       
         Invoke("RedemarrerScene", 3f);
     }
-    // Il faut appeller cette fonction dans la collision avec le yéti.
+
 
     void DeconnecterCamera()
     {
         Camera.main.GetComponent<PositionConstraint>().enabled = false;
     }
+    
     void RedemarrerScene()
     {
         string nomSceneCourante = SceneManager.GetActiveScene().name;
